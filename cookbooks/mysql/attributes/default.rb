bf8e43af1be70c7e9c2cbdb80ba42e328efa85ac
hiroki nigorinuma <nigohiroki@gmail.com>
2013-06-28 11:12:33 +0900

    update mysql version

---
+++ cookbooks/mysql/attributes/default.rb
@@ -0,0 +1,2 @@
+default['mysql']['version']   = "5.5.30-1.el5.remi"
+default['mysql']['server-id'] = "0"
--- cookbooks/mysql/recipes/default.rb
+++ cookbooks/mysql/recipes/default.rb
@@ -1,9 +1,11 @@
-%w{mysql mysql-server}.each do |package_name|
+%w{mysql mysql-server mysql-devel}.each do |package_name|
   package package_name do
     action :install
+    version node['mysql']['version']
   end
 end
 
 service "mysqld" do
+  supports :status => true, :restart => true, :reload => true
   action [ :enable, :start ]
 end
